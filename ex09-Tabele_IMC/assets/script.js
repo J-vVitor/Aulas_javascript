ec7d5c290874319bfadc0c06235046d82a2902a4
João Vitor <132713260+J-vVitor@users.noreply.github.com>
2023-07-03 20:31:23 -0300

    ex

--- ex09-Tabele_IMC/assets/script.js
+++ ex09-Tabele_IMC/assets/script.js
@@ -5,4 +5,9 @@ const formulario = document.querySelector('.formulario');
 formulario.addEventListener('submit',function(event){
     event.preventDefault();
     console.log('evento previnido!');
+
+    const peso = document.querySelector('#peso').value;
+    const altura = document.querySelector('#altura').value;
+    const resultado = document.querySelector('#resultadofinal')
+    
 });
