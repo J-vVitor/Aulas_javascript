2195b174affb4f38f6e7ad4ae75a820694e8e5f0
João Vitor <132713260+J-vVitor@users.noreply.github.com>
2023-08-29 16:33:40 -0300

    menu_teste

---
+++ menu-hamburguinho/assets/script.js
@@ -0,0 +1,11 @@
+var clique = document.querySelector('#clique')
+var list = document.querySelector('#list')
+
+function cliquelist(){
+    if(list.style.display == 'none'){
+        list.style.display = 'block'
+    }
+    else{
+        list.style.display = 'none'
+    }
+}
