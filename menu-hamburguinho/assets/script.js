bcd35ddce9b049b4028842ce73308092fb9a53f4
João Vitor <132713260+J-vVitor@users.noreply.github.com>
2023-08-29 20:12:35 -0300

    menu

--- menu-hamburguinho/assets/script.js
+++ menu-hamburguinho/assets/script.js
@@ -1,6 +1,9 @@
 var clique = document.querySelector('#clique')
 var list = document.querySelector('#list')
 
+
+document.write ("<h1>Menu Hamburguinho</h1>")
+
 function cliquelist(){
     if(list.style.display == 'none'){
         list.style.display = 'block'
