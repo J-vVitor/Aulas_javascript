da950813b141779892e1e73436156e8c669eea90
João Vitor <132713260+J-vVitor@users.noreply.github.com>
2023-08-30 16:39:43 -0300

    Calculadora_IMC

---
+++ basico/condicoes.js
@@ -0,0 +1,8 @@
+var idadePedro = 23;
+
+if(idadePedro < 20 || idadePedro < 24 ){ // OU : PELO MENOS UMA CONDIÇÃO VERDADEIRA;
+    console.log('entre 20 e 23')
+}
+if(idadePedro > 20 && idadePedro < 24 ){ // E : Verdadeiro se todos forem true;
+    console.log('entre 20 e 23')
+}
---
+++ calculadora_imc/assets/script.js
@@ -0,0 +1,27 @@
+var altura = document.querySelector('#altura');
+var peso = document.querySelector('#peso');
+var divResultados = document.querySelector('#resultados');
+
+
+
+function envio(){
+    var conversao = altura.value / 100;
+    var imc = (peso.value) / (altura.value * altura.value)
+
+    
+    if(imc < 18.5){
+        divResultados.innerHTML = `Você possui índice de massa corporal igual a ${imc.toFixed(2)}, sendo classificado como: ABAIXO DO PESO`//abaixo do normal
+       }else if(imc >= 18.5 && imc <= 24.5){
+        divResultados.innerHTML = `Você possui índice de massa corporal igual a ${imc.toFixed(2)}, sendo classificado como: NORMAL` //normal
+       }else if(imc >= 25 && imc <= 29.9){
+        divResultados.innerHTML = `Você possui índice de massa corporal igual a ${imc.toFixed(2)}, sendo classificado como: SOBREPESO`//sobrepeso
+       }else if(imc >=30 && imc <=34.9 ){
+        divResultados.innerHTML = `Você possui índice de massa corporal igual a ${imc.toFixed(2)}, sendo classificado como: OBESIDADE GRAU 1 `//obg 1 
+       }else if(imc >=35 && imc <=39.9 ){
+        divResultados.innerHTML = `Você possui índice de massa corporal igual a ${imc.toFixed(2)}, sendo classificado como: OBESIDADE GRAU 2 ` //obg 2 
+       }else if(imc >=40 ){
+        divResultados.innerHTML = `Você possui índice de massa corporal igual a ${imc.toFixed(2) }, sendo classificado como: OBESIDADE GRAU 3 `//ob3
+       }else{
+        alert('Digite algum número para o IMC ser calculado!')
+       }
+    }
