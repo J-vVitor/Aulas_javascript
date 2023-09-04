77d3a4b000446724e9453682d70e791a6d2627b7
João Vitor <132713260+J-vVitor@users.noreply.github.com>
2023-09-04 19:37:56 -0300

    f

--- basico/lacos_repeticao/script.js
+++ basico/lacos_repeticao/script.js
@@ -1,8 +1,23 @@
-x = 1;
+/*x = 1
 
-while(x <10){
-    console.log(x+1)
+while(x <= 10){
+    
+    console.log(x)
+
+    if(x === 5){
+        break
+    }
     
     x++
 
+} */
+
+var palavra = 'vitor';
+
+i = 0;
+
+while(i<=4){
+    console.log(palavra[i])
+
+    i++
 }
